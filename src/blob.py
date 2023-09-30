1b3fecdf8936aa960619ceda9e92ae52999b534e
pavalso <92272697+pavalso@users.noreply.github.com>
2023-09-30 13:08:50 +0200

    Created blob API class

---
+++ src/blob.py
@@ -0,0 +1,38 @@
+import uuid
+
+
+try:
+    from db.dao import DAO
+    from classes._file_blob import _FileBlob
+except ImportError:
+    from src.db.dao import DAO
+    from src.classes._file_blob import _FileBlob
+
+
+class Blob(_FileBlob):
+
+    def __init__(self, id: str, owner = None, public: bool = False, allowedUsers: list = None) -> None:
+        super().__init__(id)
+
+        self.owner = owner
+        self.public = public
+        self.allowedUsers = allowedUsers if allowedUsers else []
+
+    @staticmethod
+    def create(owner, public: bool = False, allowedUsers: list = None) -> 'Blob':
+        _b = Blob(str(uuid.uuid4()), owner, public, allowedUsers)
+        DAO.newBlob(_b.id, _b.owner, _b.public)
+        return _b
+
+    def read(self) -> bytes:
+        return super().read()
+    
+    def write(self, data: bytes) -> None:
+        super().write(data)
+
+    def delete(self) -> None:
+        super().delete()
+        DAO.deleteBlob(self.id)
+
+    def save(self) -> None:
+        DAO.updateBlob(self.id, self.owner, self.public)
