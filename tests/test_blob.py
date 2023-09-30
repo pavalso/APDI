c8de6dde54e73b9d2b582b79bcc110a8fe4d87e4
pavalso <92272697+pavalso@users.noreply.github.com>
2023-09-30 11:13:12 +0200

    Implement read test

--- tests/test_blob.py
+++ tests/test_blob.py
@@ -2,3 +2,12 @@ import pytest
 import os
 
 from src.classes.file_blob import FileBlob
+
+
+@pytest.fixture
+def default_blob():
+    return FileBlob('123456', 'me')
+
+def test_empty_read(default_blob):
+    assert default_blob.read() == b''
+
