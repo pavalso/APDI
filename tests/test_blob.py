59ca294c856c3f211faaf300e1fc003c4db2da1d
pavalso <92272697+pavalso@users.noreply.github.com>
2023-09-30 11:13:27 +0200

    Implement write test

--- tests/test_blob.py
+++ tests/test_blob.py
@@ -11,3 +11,14 @@ def default_blob():
 def test_empty_read(default_blob):
     assert default_blob.read() == b''
 
+def test_chain_write(default_blob):
+    _bytes = b'123456' * 1000
+    assert default_blob.write(_bytes) == len(_bytes)
+    _bytes = b'abcd'
+    assert default_blob.write(_bytes) == len(_bytes)
+
+def test_write_read(default_blob):
+    _bytes = b'123456' * 1000
+    default_blob.write(_bytes)
+    assert default_blob.read() == _bytes
+
