1baccf47d0353a6273d8576a7d37a5b5c8bba835
pavalso <92272697+pavalso@users.noreply.github.com>
2023-09-30 13:11:40 +0200

    Implement close and reorganize class

--- src/db/dao.py
+++ src/db/dao.py
@@ -1,8 +1,5 @@
 import sqlite3
-import enum
 
-
-_DB_NAME = 'pyblob.db'
 
 class __Dao:
 
@@ -12,35 +9,47 @@ class __Dao:
         self._conn = None
         self._cursor = None
 
-    def connect(self) -> None:
-        self._conn = sqlite3.connect(_DB_NAME)
+    def connect(self, db_name) -> None:
+        self._conn = sqlite3.connect(db_name)
         self._cursor = self._conn.cursor()
         self._cursor.execute(
-            'CREATE TABLE IF NOT EXISTS %s (id TEXT, owner TEXT, public INTEGER, PRIMARY KEY (id))' % self.BLOBS)
+            '''CREATE TABLE IF NOT EXISTS %s (   
+                id TEXT,
+                owner TEXT, 
+                public INTEGER, 
+                PRIMARY KEY (id))''' % self.BLOBS)
 
     def newBlob(self, id: str, owner: str, public: bool = False) -> None:
         self._cursor.execute(
-            'INSERT INTO %s VALUES (?, ?, ?)' % self.BLOBS, 
+            '''INSERT INTO %s 
+            VALUES (?, ?, ?)''' % self.BLOBS, 
             (id, owner, public))
         self._conn.commit()
 
     def getBlob(self, id: str) -> tuple:
         self._cursor.execute(
-            'SELECT * FROM %s WHERE id=?' % self.BLOBS, 
+            '''SELECT * 
+            FROM %s 
+            WHERE id=?''' % self.BLOBS, 
             (id,))
         return self._cursor.fetchone()
     
     def updateBlob(self, id: str, owner: str, public: bool = False) -> None:
         self._cursor.execute(
-            'UPDATE %s SET owner=?, public=? WHERE id=?' % self.BLOBS, 
+            '''UPDATE %s 
+            SET owner=?, public=? 
+            WHERE id=?''' % self.BLOBS, 
             (owner, public, id))
         self._conn.commit()
 
     def deleteBlob(self, id: str) -> None:
         self._cursor.execute(
-            'DELETE FROM %s WHERE id=?' % self.BLOBS,
+            '''DELETE FROM %s 
+            WHERE id=?''' % self.BLOBS,
             (id,))
         self._conn.commit()
 
+    def close(self) -> None:
+        self._conn.close()
+
 DAO = __Dao()
-DAO.connect()
