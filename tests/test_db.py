7d5517296a116b8f93b9930891e183dfd272b856
pavalso <92272697+pavalso@users.noreply.github.com>
2023-09-30 14:44:11 +0200

    Implement tests close, fetch and insert existing

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,8 +1,10 @@
+import src.exceptions as exceptions
+
 import unittest
 
 from src.db.dao import DAO
 from src import Blob
-from sqlite3 import IntegrityError
+from sqlite3 import IntegrityError, ProgrammingError
 
 
 class TestDB(unittest.TestCase):
@@ -10,6 +12,7 @@ class TestDB(unittest.TestCase):
     def setUp(self):
         DAO.connect(':memory:')
         self.default_blob = Blob('x', 'me')
+        self.default_blob._insert()
 
     @staticmethod
     def _raw_select(id):
@@ -19,7 +22,6 @@ class TestDB(unittest.TestCase):
     @staticmethod
     def _raw_insert(id, owner, public):
         DAO._cursor.execute('INSERT INTO blobs VALUES (?, ?, ?)', (id, owner, public))
-        DAO._conn.commit()
     
     @staticmethod
     def _exists_in_db(id):
@@ -31,25 +33,38 @@ class TestDB(unittest.TestCase):
         self.assertEqual(_b.owner, self.default_blob.owner)
         self.assertTrue(TestDB._exists_in_db(_b.id))
 
+    def test_insert_on_existing_blob(self):
+        self.assertIsNone(self.default_blob._insert())
+
     def test_id_readonly(self):
         _b = Blob.create(self.default_blob.owner)
         self.assertRaises(AttributeError, setattr, _b, 'id', '123456')
 
     def test_unique_id(self):
-        TestDB._raw_insert(self.default_blob.id, None, None)
         self.assertRaises(IntegrityError, TestDB._raw_insert, self.default_blob.id, None, None)
 
     def test_update_blob(self):
         _b = Blob.create(self.default_blob.owner)
         _old_owner = _b.owner
         _b.owner = _old_owner + '_new'
-        _b.save()
+        _b.update()
         self.assertNotEqual(TestDB._raw_select(_b.id)[0][1], _old_owner)
+
+    def test_fetch_blob(self):
+        _b = Blob.create(self.default_blob.owner)
+        _fetched = Blob.fetch(_b.id)
+        self.assertEqual(_b.id, _fetched.id)
+
+    def test_fetch_missing_blob(self):
+        self.assertRaises(exceptions.BlobNotFoundError, Blob.fetch, 'not_existing')
 
     def test_delete_blob(self):
         self.default_blob.delete()
         self.assertFalse(TestDB._exists_in_db(self.default_blob.id))
 
+    def test_close(self):
+        DAO.close()
+        self.assertRaises(ProgrammingError, DAO.getBlob, 'x')
+
     def tearDown(self):
-        DAO._cursor.close()
-        DAO._conn.close()
+        DAO.close()
