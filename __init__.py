dd1aed870addbdfb4f2c86db938374c60322c95e
pavalso <pablo.valver@alu.uclm.es>
2023-10-02 19:47:06 +0200

    Fix coverage

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
 from src import services
 from src.objects import Visibility
 from src.db import connect
+from src.entities import Blob
 
-connect('pyapdi.db')
+if __name__ == '__main__':
+    connect('pyapdi.db')
 
-blob = services.get_blob('9bdb2341-7c05-4289-a4b0-2b17ac5cced5')
+    blob = services.get_blob('9bdb2341-7c05-4289-a4b0-2b17ac5cced5')
 
-blob.open()
+    print(hash(blob), blob.read())
 
-print(hash(blob), blob.read())
+    blob.write(b'Hasta la vista, baby!')
+
+    _blob = Blob('9bdb2341-7c05-4289-a4b0-2b17ac5cced5', 'me')
+
+    print(hash(_blob), _blob.read())
