24a8f3a7860a2352f4608ba2bb8f2808750a1472
pavalso <92272697+pavalso@users.noreply.github.com>
2024-01-31 00:17:04 +0100

    Apply Config script created

--- mnt/apply_config.sh
+++ mnt/apply_config.sh
@@ -6,3 +6,6 @@ kubectl apply -f .\mnt\data\blob_deployment.yaml
 kubectl apply -f .\mnt\data\blob_service.yaml
 kubectl apply -f .\mnt\data\nginx_deployment.yaml
 kubectl apply -f .\mnt\data\nginx_service.yaml
+minikube ssh "sudo mkdir -p /mnt/data/public/html"
+minikube cp ./shared/html/index.html /mnt/data/public/html/index.html
+minikube cp ./shared/html/custom_404.html /mnt/data/public/html/custom_404.html
